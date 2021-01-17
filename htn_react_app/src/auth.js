41a4aee7099801de7b5514a04f758c49984d7ef6
Erfan Gh <erfanghafoori@gmail.com>
2021-01-16 22:46:26 -0500

    Create auth.js
    -authentication/google sign in

---
+++ htn_react_app/src/auth.js
@@ -0,0 +1,59 @@
+var provider = new firebase.auth.GoogleAuthProvider();
+
+//optional (Specify additional OAuth 2.0 scopes that you want to request from the authentication provider. To add a scope, call addScope)
+provider.addScope('https://www.googleapis.com/auth/contacts.readonly');
+
+//optional (extra paramters etc)
+provider.setCustomParameters({
+    'login_hint': 'user@example.com'
+  });
+
+/*
+// pop sign in 
+firebase.auth()
+  .signInWithPopup(provider)
+  .then((result) => {
+    // @type {firebase.auth.OAuthCredential} 
+    var credential = result.credential;
+
+    // This gives you a Google Access Token. You can use it to access the Google API.
+    var token = credential.accessToken;
+    // The signed-in user info.
+    var user = result.user;
+    // ...
+  }).catch((error) => {
+    // Handle Errors here.
+    var errorCode = error.code;
+    var errorMessage = error.message;
+    // The email of the user's account used.
+    var email = error.email;
+    // The firebase.auth.AuthCredential type that was used.
+    var credential = error.credential;
+    // ...
+  }); */
+
+// redirect sign in 
+firebase.auth()
+  .getRedirectResult()
+  .then((result) => {
+    if (result.credential) {
+      /** @type {firebase.auth.OAuthCredential} */
+      var credential = result.credential;
+
+      // This gives you a Google Access Token. You can use it to access the Google API.
+      var token = credential.accessToken;
+      // ...
+    }
+    // The signed-in user info.
+    var user = result.user;
+  }).catch((error) => {
+    // Handle Errors here.
+    var errorCode = error.code;
+    var errorMessage = error.message;
+    // The email of the user's account used.
+    var email = error.email;
+    // The firebase.auth.AuthCredential type that was used.
+    var credential = error.credential;
+    // ...
+  });
+
