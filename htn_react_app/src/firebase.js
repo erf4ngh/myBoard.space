3f8a5114a84f43228853e200934d18b922bb2dee
Erfan Gh <erfanghafoori@gmail.com>
2021-01-17 02:50:42 -0500

    Fixed firebase auth
    -firebase auth inside firebase.js
    -deprecate auth.js

--- htn_react_app/src/firebase.js
+++ htn_react_app/src/firebase.js
@@ -13,3 +13,21 @@ const config = {
 
 const app = firebase.initializeApp(config)
 export default app;
+
+export const auth = firebase.auth();
+const googleProvider = new firebase.auth.GoogleAuthProvider()
+export const signInWithGoogle = () => {
+  auth.signInWithPopup(googleProvider).then((res) => {
+    console.log(res.user)
+  }).catch((error) => {
+    console.log(error.message)
+  })
+}
+
+export const logOut = () => {
+    auth.signOut().then(()=> {
+      console.log('logged out')
+    }).catch((error) => {
+      console.log(error.message)
+    })
+  }
