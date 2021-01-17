7afcf0f468fc7c0622665ddd52e2e6cbb39a6abd
Andrew Dong <a28dong@edu.uwaterloo.ca>
2021-01-16 20:54:32 -0500

    Finished homepage and added new assets

--- htn_react_app/src/components/HOMEPAGE/login.jsx
+++ htn_react_app/src/components/HOMEPAGE/login.jsx
@@ -9,6 +9,7 @@ const responseGoogle = (response) => {
 const GoogleLogDiv = () => {
     return (    
         <GoogleLogin
+            style={{width:"100px"}}
             clientId="658977310896-knrl3gka66fldh83dao2rhgbblmd4un9.apps.googleusercontent.com"
             buttonText="Login"
             onSuccess={responseGoogle}
