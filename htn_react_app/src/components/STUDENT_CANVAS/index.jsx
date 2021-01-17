42cdf7d3c989046d6221d16d50aaafe622c423be
Andrew Dong <a28dong@edu.uwaterloo.ca>
2021-01-17 03:14:15 -0500

    Update index.jsx for Student

--- htn_react_app/src/components/STUDENT_CANVAS/index.jsx
+++ htn_react_app/src/components/STUDENT_CANVAS/index.jsx
@@ -13,10 +13,53 @@ import Nav from "react-bootstrap/Nav";
 import ToggleButton from "react-bootstrap/ToggleButton";
 import ToggleButtonGroup from "react-bootstrap/ToggleButtonGroup";
 import app from "../../firebase";
+import Logo from '../../assets/logo2.svg';
 
 const CanvasWrapper = styled.div`
-    border: 5px solid red;
+    border: 5px solid #B0BEC9;
     margin-bottom:10px;
+`
+
+const Logoutbtn =styled(Button)`
+    padding:20 px;
+    width: 60 px;
+    &.btn-primary,&.btn-primary:active:focus, &.btn-primary:active, &.btn-primary:focus, &.btn-primary:visited {
+        border: 0px;
+        background-color: #B0BEC9; 
+        !important;
+    }
+
+    &.btn-primary:hover{
+        border: 0px;
+        color: #000000;
+        background-color: #E2F1FC; 
+        !important;
+    }
+}
+`
+
+const CustomDiv = styled.div`
+    height: 100vh;
+}
+`
+const CustomH1 = styled.h1`
+    &.title {
+        margin-top:50px;
+        font-size: 40px;
+    }
+    &.subtitle {
+        font-size: 32px;
+        font-weight: lighter;
+        margin-bottom: 65px;
+    }
+
+    &.logout {
+        font-size: 30px;
+    }
+}`
+
+const LogoImg = styled.img`
+    margin-Top:40px;
 `
 
 const StudentCanvasView = () => {
@@ -34,9 +77,26 @@ const StudentCanvasView = () => {
     },[]);
 
     return (
-        <>
-            <CanvasWrapper>
-                <CanvasDraw 
+        <CustomDiv>
+            <Container>
+                <Row> 
+                    <Col xs="auto">
+                        <LogoImg src={Logo} width="150" height="150"/>
+                    </Col>
+                    <Col className="align-self-end" xs="auto">
+                        <div>
+                            <CustomH1 className="title">myBoard</CustomH1>
+                        </div>
+                        <div>
+                            <CustomH1 className="subtitle">.space</CustomH1>
+                        </div>
+                    </Col>
+                    <Col xs="auto" className="ml-auto">
+                        <Logoutbtn variant="primary"><CustomH1 className="logout">Logout</CustomH1></Logoutbtn>
+                    </Col>
+                </Row>
+                <CanvasWrapper>
+                    <CanvasDraw 
                     hideGrid="true"
                     disabled="true"
                     ref={loadableCanvas}
@@ -44,10 +104,10 @@ const StudentCanvasView = () => {
                     canvasHeight="600px"
                     immediateLoading="false"
                     // saveData={drawData}
-                    />
-                    
-            </CanvasWrapper>
-        </>
+                    />   
+                </CanvasWrapper>
+            </Container>
+        </CustomDiv>
     )
 }
 
